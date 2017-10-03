8cae9e804e0fae02c512c5803832f595acf81474
ducong <1025434218@qq.com>
2017-10-03 12:31:07 +0000

    17

--- app/Filters/ThreadsFilters.php
+++ app/Filters/ThreadsFilters.php
@@ -6,7 +6,7 @@ use App\User;
 
 class ThreadsFilters extends Filters
 {
-    protected $filters = ['by'];
+    protected $filters = ['by', 'popular'];
     /**
      * @param $username
      * @return mixed
@@ -16,5 +16,13 @@ class ThreadsFilters extends Filters
         $user = User::where('name', $username)->firstOrFail();
 
         return $this->builder->where('user_id', $user->id);
+    }
+
+    protected function popular()
+    {
+        // 将上面的 latest() 的 orders 清空 
+        $this->builder->getQuery()->orders = [];
+
+        return $this->builder->orderBy('replies_count', 'desc');
     }
 }
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -12,8 +12,14 @@
 
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+
+    <style>
+        body { padding-bottom: 100px; }
+        .flex { flex: 1; }
+        .level { display: flex;align-items: center; }
+    </style>
 </head>
-<body style="padding-bottom: 100px">
+<body>
 <div id="app">
     <nav class="navbar navbar-default navbar-static-top">
         <div class="container">
@@ -45,6 +51,7 @@
                             @signIn
                             <li><a href="/threads?by={{ auth()->user()->name }}">My Threads</a></li>
                             @endsignIn
+                            <li><a href="/threads?popular=1">Popular Threads</a></li>
                         </ul>
                     </li>
 
