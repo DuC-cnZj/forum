d779a2221afcc5421e2f094a3826c8a6025f1129
ducong <1025434218@qq.com>
2017-10-04 08:36:23 +0000

    19

--- app/Http/Controllers/FavoritesController.php
+++ app/Http/Controllers/FavoritesController.php
@@ -21,7 +21,9 @@ class FavoritesController extends Controller
      */
     public function store(Reply $reply)
     {
-        return $reply->favorite();
+        $reply->favorite();
+
+        return back();
 //        $reply->favorites()->create(['user_id', auth()->id()]);
 //        Favorite::create([
 //            'user_id'       => auth()->id(),
