62753c4513ac0e098a80c081537c6ad959266979
ducong <1025434218@qq.com>
2017-10-09 06:43:33 +0000

    30

--- app/Favorite.php
+++ app/Favorite.php
@@ -6,5 +6,15 @@ use Illuminate\Database\Eloquent\Model;
 
 class Favorite extends Model
 {
+    use RecordsActivity;
+
+    /**
+     * @var array
+     */
     protected $guarded = [];
+
+    public function favorited()
+    {
+        return $this->morphTo();
+    }
 }
--- resources/views/threads/reply.blade.php
+++ resources/views/threads/reply.blade.php
@@ -1,4 +1,4 @@
-<div class="panel panel-default">
+<div id="reply-{{ $reply->id }}" class="panel panel-default">
     <div class="panel-heading">
         <div class="level">
             <h5 class="flex">
@@ -9,7 +9,7 @@
 
             <form action="/replies/{{ $reply->id }}/favorites" method="POST">
                 {{ csrf_field() }}
-                <button type="submit" class="btn btn-default">
+                <button type="submit" class="btn btn-default" {{ $reply->isFavorited() > 0 ? 'disabled' : '' }}>
                     {{ $reply->favorites_count }} {{ str_plural('Favorite', $reply->favorites_count) }}
                 </button>
             </form>
