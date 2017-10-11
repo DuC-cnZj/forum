e2c34bdd95bb2fcbb19ac25ee3151fcd99525808
ducong <1025434218@qq.com>
2017-10-11 01:50:48 +0000

    33

--- public/js/app.js
+++ public/js/app.js
@@ -1720,6 +1720,13 @@ Object.defineProperty(__webpack_exports__, "__esModule", { value: true });
             this.editing = false;
 
             flash('updated!');
+        },
+        destroy: function destroy() {
+            axios.delete('/replies/' + this.attributes.id);
+
+            $(this.$el).fadeOut(300);
+
+            flash('deleted!');
         }
     }
 });
--- resources/assets/js/components/Reply.vue
+++ resources/assets/js/components/Reply.vue
@@ -16,6 +16,13 @@
                 this.editing = false;
 
                 flash('updated!');
+            },
+            destroy() {
+                axios.delete('/replies/' + this.attributes.id);
+
+                $(this.$el).fadeOut(300, () => {
+                    flash('deleted!');
+                });
             }
         }
     }
